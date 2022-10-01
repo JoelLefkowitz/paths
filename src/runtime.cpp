e5d3d29f2e2a8915e4d307e58516510346f099b8
Joel Lefkowitz <joellefkowitz@hotmail.com>
2022-10-01 22:20:51 +0100

    Debug cxx11:abi

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -1,89 +1,89 @@
-// ʕ •ᴥ•ʔ Paths - runtime.cpp ʕ•ᴥ• ʔ
-// OS specific path manipulation including retrieving the executable's path.
-// https://github.com/joellefkowitz/paths
-// Version: 0.1.0
-// License: MIT
+// // ʕ •ᴥ•ʔ Paths - runtime.cpp ʕ•ᴥ• ʔ
+// // OS specific path manipulation including retrieving the executable's path.
+// // https://github.com/joellefkowitz/paths
+// // Version: 0.1.0
+// // License: MIT
 
-#include "runtime.hpp"
-#include "detect.hpp"
-#include "paths.hpp"
-#include "strings.hpp"
-#include <string>
+// #include "runtime.hpp"
+// #include "detect.hpp"
+// #include "paths.hpp"
+// #include "strings.hpp"
+// #include <string>
 
-#if PLATFORM_DETECTED_OS == PLATFORM_LINUX
+// #if PLATFORM_DETECTED_OS == PLATFORM_LINUX
 
-#include <linux/limits.h>
-#include <unistd.h>
+// #include <linux/limits.h>
+// #include <unistd.h>
 
-std::string paths::filename() {
-    char buffer[PATH_MAX];
-    readlink("/proc/self/exe", buffer, PATH_MAX);
-    return buffer;
-}
+// std::string paths::filename() {
+//     char buffer[PATH_MAX];
+//     readlink("/proc/self/exe", buffer, PATH_MAX);
+//     return buffer;
+// }
 
-#elif (PLATFORM_DETECTED_OS == PLATFORM_DARWIN || PLATFORM_DETECTED_OS == PLATFORM_IOS)
+// #elif (PLATFORM_DETECTED_OS == PLATFORM_DARWIN || PLATFORM_DETECTED_OS == PLATFORM_IOS)
 
-#include <mach-o/dyld.h>
+// #include <mach-o/dyld.h>
 
-std::string paths::filename() {
-    auto bufsize = static_cast<uint32_t>(PATH_MAX);
+// std::string paths::filename() {
+//     auto bufsize = static_cast<uint32_t>(PATH_MAX);
 
-    char buffer[bufsize];
+//     char buffer[bufsize];
 
-    if (_NSGetExecutablePath(buffer, &bufsize) == -1) {
-        throw std::length_error(
-            "Filename exceeds maximum path length: " + std::to_string(PATH_MAX)
-        );
-    }
+//     if (_NSGetExecutablePath(buffer, &bufsize) == -1) {
+//         throw std::length_error(
+//             "Filename exceeds maximum path length: " + std::to_string(PATH_MAX)
+//         );
+//     }
 
-    return realpath(buffer, NULL);
-}
+//     return realpath(buffer, NULL);
+// }
 
-#elif PLATFORMS_DETECTED_OS == PLATFORM_WINDOWS
+// #elif PLATFORMS_DETECTED_OS == PLATFORM_WINDOWS
 
-#include <string>
-#include <windows.h>
+// #include <string>
+// #include <windows.h>
 
-std::string paths::filename() {
-    wchar_t buffer[MAX_PATH];
-    auto    size = GetModuleFileNameW(NULL, buffer, MAX_PATH);
+// std::string paths::filename() {
+//     wchar_t buffer[MAX_PATH];
+//     auto    size = GetModuleFileNameW(NULL, buffer, MAX_PATH);
 
-    if (size > MAX_PATH + 1) {
-        throw std::length_error(
-            "Filename exceeds maximum path length: " + std::to_string(MAX_PATH)
-        );
-    }
+//     if (size > MAX_PATH + 1) {
+//         throw std::length_error(
+//             "Filename exceeds maximum path length: " + std::to_string(MAX_PATH)
+//         );
+//     }
 
-    std::wstring ws(buffer);
+//     std::wstring ws(buffer);
 
-    return std::string(ws.begin(), ws.end());
-}
+//     return std::string(ws.begin(), ws.end());
+// }
 
-#elif PLATFORMS_DETECTED_OS == PLATFORM_BSD
+// #elif PLATFORMS_DETECTED_OS == PLATFORM_BSD
 
-std::string paths::filename() {
-    return "";
-}
+// std::string paths::filename() {
+//     return "";
+// }
 
-#elif PLATFORMS_DETECTED_OS == PLATFORM_SOLARIS
+// #elif PLATFORMS_DETECTED_OS == PLATFORM_SOLARIS
 
-std::string paths::filename() {
-    return "";
-}
+// std::string paths::filename() {
+//     return "";
+// }
 
-#elif PLATFORMS_DETECTED_OS == PLATFORM_ANDROID
+// #elif PLATFORMS_DETECTED_OS == PLATFORM_ANDROID
 
-std::string paths::filename() {
-    char buffer[PATH_MAX];
-    readlink("/proc/self/maps", buffer, PATH_MAX);
-    // realpath(path, buffer);
-    // munmap(begin, offset);
-    // fclose(maps);
-    return buffer;
-}
+// std::string paths::filename() {
+//     char buffer[PATH_MAX];
+//     readlink("/proc/self/maps", buffer, PATH_MAX);
+//     // realpath(path, buffer);
+//     // munmap(begin, offset);
+//     // fclose(maps);
+//     return buffer;
+// }
 
-#endif
+// #endif
 
-std::string paths::dirname() {
-    return tail(filename());
-}
+// std::string paths::dirname() {
+//     return tail(filename());
+// }
--- test/runtime/filename.cpp
+++ test/runtime/filename.cpp
@@ -1,11 +1,11 @@
-#include "../../src/runtime.hpp"
-#include <cstdlib>
-#include <gtest/gtest.h>
+// #include "../../src/runtime.hpp"
+// #include <cstdlib>
+// #include <gtest/gtest.h>
 
-TEST(Paths_Runtime, filename) {
-    if (const char *expected = std::getenv("FILENAME")) {
-        // EXPECT_EQ(paths::filename(), expected);
-    } else {
-        throw std::runtime_error("Missing FILENAME environment variable");
-    }
-}
+// TEST(Paths_Runtime, filename) {
+//     if (const char *expected = std::getenv("FILENAME")) {
+//         EXPECT_EQ(paths::filename(), expected);
+//     } else {
+//         throw std::runtime_error("Missing FILENAME environment variable");
+//     }
+// }
