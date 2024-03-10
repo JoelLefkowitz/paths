7f51ee60f4696e27b92dfae1c1627c02a31de926
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-03-10 21:33:19 +0000

    Let scons pick flags

--- SConstruct.py
+++ SConstruct.py
@@ -2,13 +2,11 @@ import re
 import os
 import psutil
 from walkmate import get_child_files
-from emoji import emojize
 from SCons.Environment import Environment
 from SCons.Script import AddOption
 
 env = Environment(
     LIBS=["gtest", "pthread"],
-    CXXFLAGS=["-std=c++17"],
     CPPPATH=os.getenv("CPPPATH", "/usr/include"),
     LIBPATH=os.getenv("LIBPATH", "/usr/lib"),
     CXXCOMSTR="Compiling $TARGET",
@@ -16,29 +14,32 @@ env = Environment(
     num_jobs=psutil.cpu_count(),
 )
 
-warnings = [
-    "-Wall",
-    "-Wconversion",
-    "-Wextra",
-    "-Wmissing-declarations",
-    "-Wpedantic",
-    "-Wshadow-uncaptured-local",
-    "-Wshadow",
-    "-Wno-deprecated-declarations",
-    "-Wno-macro-redefined",
-    "-Wno-missing-braces",
-    "-Wno-unknown-warning-option",
-    "-Wno-unused-parameter",
-    "-Wno-vla-extension",
-    "-Wno-everything",
-]
-
 AddOption("--iwyu", action="store_true")
 AddOption("--typecheck", action="store_true")
 
-if env["PLATFORM"] == "darwin":
+if env["PLATFORM"] == "windows":
+    env["CXX"] = "g++"
+    env["CXXFLAGS"] = ["/std=c++17"]
+
+else:
     env["CXX"] = "clang++"
-    env["CXXFLAGS"].extend(warnings)
+    env["CXXFLAGS"] = [
+        "-std=c++17",
+        "-Wall",
+        "-Wconversion",
+        "-Wextra",
+        "-Wmissing-declarations",
+        "-Wpedantic",
+        "-Wshadow-uncaptured-local",
+        "-Wshadow",
+        "-Wno-deprecated-declarations",
+        "-Wno-macro-redefined",
+        "-Wno-missing-braces",
+        "-Wno-unknown-warning-option",
+        "-Wno-unused-parameter",
+        "-Wno-vla-extension",
+        "-Wno-everything",
+    ]
 
 if GetOption("iwyu"):
     env["CXX"] = "include-what-you-use"
