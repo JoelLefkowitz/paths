f4863546737b610ee48eaff049e79557dda14027
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-03-11 19:05:04 +0000

    Add mingw

--- SConstruct.py
+++ SConstruct.py
@@ -5,16 +5,10 @@ from SCons.Environment import Environment
 from SCons.Script import AddOption
 from walkmate import get_child_files as tree
 
-env = Environment(
-    LIBS=["gtest", "pthread"],
-    ENV={"PATH": os.getenv("PATH", [])},
-    CXXFLAGS=["-std=c++17"],
-    CPPPATH=os.getenv("CPPPATH", "/usr/include").split(","),
-    LIBPATH=os.getenv("LIBPATH", "/usr/lib").split(","),
-    # CXXCOMSTR="Compiling $TARGET",
-    # LINKCOMSTR="Linking $TARGET",
-    num_jobs=psutil.cpu_count(),
-)
+libs = [
+    "gtest",
+    "pthread",
+]
 
 warnings = [
     "-Wall",
@@ -33,23 +27,37 @@ warnings = [
     "-Wno-everything",
 ]
 
+env = Environment(
+    LIBS=libs,
+    ENV={"PATH": os.getenv("PATH", [])},
+    CXXFLAGS=["-std=c++17"],
+    CPPPATH=os.getenv("CPPPATH", "/usr/include").split(","),
+    LIBPATH=os.getenv("LIBPATH", "/usr/lib").split(","),
+    # CXXCOMSTR="Compiling $TARGET",
+    # LINKCOMSTR="Linking $TARGET",
+    num_jobs=psutil.cpu_count(),
+)
+
 AddOption("--iwyu", action="store_true")
 AddOption("--typecheck", action="store_true")
 
 if env["PLATFORM"] == "win32":
     env["CXX"] = "g++"
     env["LIBS"].remove("pthread")
+    env["tools"] = ["mingw"]
+
+elif GetOption("typecheck"):
+    env["LINK"] = ":"
+    env["CXXFLAGS"].extend(["-S", "-O0"])
+
+elif GetOption("iwyu"):
+    env["CXX"] = "include-what-you-use"
+
 else:
     env["CXX"] = "clang++"
     env["CXXFLAGS"].extend(warnings)
 
-if GetOption("iwyu"):
-    env["CXX"] = "include-what-you-use"
-
-if GetOption("typecheck"):
-    env["LINK"] = ":"
-    env["CXXFLAGS"].extend(["-S", "-O0"])
-
-sources = lambda pattern: [i for i in tree(".") if re.search(pattern, i)]
-
-env.Program(target="dist/tests", source=sources("\.cpp$"))
+env.Program(
+    target="dist/tests",
+    source=[i for i in tree(".") if re.search("\.cpp$", i)],
+)
